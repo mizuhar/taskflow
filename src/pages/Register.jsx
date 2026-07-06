4f6b9b71a3358c27b3e53ee6ec23661461faeaad
mizuhar <mizuhar@abv.bg>
2026-07-06 17:56:38 +0300

    first register account

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import { register } from "../services/firebase";
+
 function Register() {
-  return <h1>Register</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const userCredential = await register(email, password);
+      console.log("Registered user:", userCredential.user);
+    } catch (error) {
+      console.error("Register error:", error.message);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Register</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
